886e33f2eca7e0878d7b61aad6551508bc63fc3f
piwe12 <54009962+piwe12@users.noreply.github.com>
2020-07-31 16:31:16 +0700

    ini buat java button grupband maroon 5 gitar

---
+++ maroon_gitar.java
@@ -0,0 +1,14 @@
+package com.example.nisulo4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class maroon_gitar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maroon_gitar);
+    }
+}
