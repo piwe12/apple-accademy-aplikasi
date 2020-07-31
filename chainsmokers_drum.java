2b4e562483cdce9d5b2aa5e41f381eff37a75094
piwe12 <54009962+piwe12@users.noreply.github.com>
2020-07-31 17:03:20 +0700

    ini buat java button grupband chainsmokers drum

---
+++ chainsmokers_drum.java
@@ -0,0 +1,14 @@
+package com.example.nisulo4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class chainsmokers_drum extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chainsmokers_drum);
+    }
+}
